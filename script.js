698b77c4566a07ab9a7d3d359584a6c3742c0811
brobertson3 <brobertson3@users.noreply.github.com>
2017-10-31 22:15:56 -0700

    Changes to validate inpur values
    Also added the ability for the user to guess the entire word at any time.

--- script.js
+++ script.js
@@ -11,55 +11,104 @@ var outcomeElem = document.getElementById("outcome");
 var initialOutcome = outcomeElem.textContent;
 var restartButton = document.getElementById("restart");
 var userElem = document.getElementById("userinput");
+var p2ErrorElem = document.querySelector('#player2 p.error');
+var p1ErrorElem = document.querySelector('#player1 p.error');
+var answerElem = document.getElementById("word");
 
 restartButton.addEventListener("click", restartGame);
 submitWordButton.addEventListener("click", createNewAnswer);
 init();
 
-/* TODO 1. Validation of input fields
+/* TODO CHECK 1. Validation of input fields
  *      2. Draw Hangman figure
  *      3. 
  */
- 
 
 function init() {
 	document.getElementById("player1").style.display = "block";
 	document.getElementById("player2").style.display = "none";
-	// submitButton.addEventListener("click", validateInput);
-	submitButton.addEventListener("click", compareAndDisplayOutcome);
+    submitButton.addEventListener("click", validateInput);
+	//submitButton.addEventListener("click", compareAndDisplayOutcome);
 }
 
-function validateInput(input) {
-	// If not the entire correct answer should only be one letter at a time
-	if (input.length === 1) {
-		compareAndDisplayOutcome();	
-	} else if (input.length > 1) {
-		
+function validateInput() {
+	var input = userElem.value;
+	p2ErrorElem.innerHTML = "";
+	userElem.value = "";
+	// If all letters
+	if (validateAllLetters(input)) {
+		// If not the entire correct answer, should only be one letter at a time
+		if (input.length === 1) {
+			compareAndDisplayOutcome(input);	
+		} else if (input.length === correctAnswer.length) {
+			areTheyAWinner(input);
+		} else {
+			//  Not a valid response, output message saying so
+			p2ErrorElem.innerHTML = "Not a valid input. Enter either a single letter or the entire word.";
+		}
 	} else {
+		p2ErrorElem.innerHTML = "Invalid input. You can only enter letters.";
+	}
+}
+	
+// Make sure that the input is all letters
+function validateAllLetters(input) {
+	console.log(input);
+	if (input.match(/^[A-Za-z]+$/))
+		return true;
+	else
+		return false;
+}
 
+// Validate that the proposed answer is only made of letters
+function validateAnswer(answer) {
+	if (validateAllLetters(answer) && answer.length > 0)
+		return true;
+	else
+		return false;
+}
+
+function areTheyAWinner(input) {
+	// Check to see if the input is the correct answer when guessing the entire word
+	if (input === correctAnswer) { // Won
+		outcomeElem.textContent = "You win!!!";
+		for (var i = 0; i < correctAnswer.length; i++) {
+			document.getElementById('guess_area').children[i].innerHTML = "_" + correctAnswer.charAt(i) + "_";  
+		}
+	} else { // Lost
+		outcomeElem.textContent = "You lose!!!";
 	}
 
-}
-
-function validateAnswer() {
-	// Validate that the input word is only made of letters.
+	// Remove event listener to end the game
+    submitButton.removeEventListener("click", validateInput);
 }
 
 function createNewAnswer () {
-	correctAnswer = document.getElementById("word").value;
-	charsLeft = correctAnswer.length;
 
-	var htmlString = '<span class="letter">___</span>  ';
+	p1ErrorElem.innerHTML = "";
+	correctAnswer = answerElem.value;
+	// Clear the input field
+	answerElem.value = "";
+	console.log(correctAnswer);
 
-	for (var i = 0; i < correctAnswer.length - 1; i++) {
-		htmlString = htmlString + '<span class="letter">___</span>  ';
+	// Check to make sure the answer is valid
+	if (validateAnswer(correctAnswer)) { // Answer valid, continue as usual
+		console.log("answer is valid");
+		charsLeft = correctAnswer.length;
+
+		var htmlString = '<span class="letter">___</span>  ';
+
+		for (var i = 0; i < correctAnswer.length - 1; i++) {
+			htmlString = htmlString + '<span class="letter">___</span>  ';
+		}
+
+		document.getElementById("guess_area").innerHTML = htmlString;
+
+		document.getElementById("player2").style.display = "block";
+		document.getElementById("player1").style.display = "none";
+	} else {  //Answer is not valid, error message and reenter the message
+		p1ErrorElem.innerHTML = "This is not a valid input.  It must be all letters.";
 	}
-
-	document.getElementById("guess_area").innerHTML = htmlString;
-
-	document.getElementById("player2").style.display = "block";
-	document.getElementById("player1").style.display = "none";
-
 }
 
 function restartGame() {
@@ -73,9 +122,9 @@ function restartGame() {
 
 }
 
-function compareAndDisplayOutcome() {
+function compareAndDisplayOutcome(userInput) {
 
-    var userInput = userElem.value;
+    // var userInput = userElem.value;
 	var change = 0;
 	var right = false;
 	var duplicate = false;
@@ -111,19 +160,7 @@ function compareAndDisplayOutcome() {
     } else {
         outcomeElem.textContent = "You lose!!!";
         //Remove the event listener to end game
+        submitButton.removeEventListener("click", validateInput);
     }
-
-    // Original
-    // if (correctAnswer === userInput) {
-    //     outcomeElem.textContent = "You win!!!";
-    // } else if ( guessesRemaining > 0 ) {
-    //     outcomeElem.textContent = "Wrong, guess again! Guesses left: " + guessesRemaining;
-    //     guessesRemaining--;
-    // } else {
-    //     outcomeElem.textContent = "You lose!!!";
-    // }
-
-    // userInput = "";
-    userElem.value = "";
 
 }
