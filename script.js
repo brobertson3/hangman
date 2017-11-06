f0040d91c53891ff310f88f7941cf2e7c4c77b3a
brobertson3 <brobertson3@users.noreply.github.com>
2017-11-05 17:57:47 -0800

    Changes so that stick figure is drawn as the user guesses incorrectly

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 var correctAnswer = "";
-var guessesRemaining = 5;
+var guessesRemaining = 6;
 var attempt = ""; //attempted filled in string guess
 var charsLeft;
 var allGuesses = [];
+var isCanvas;
+var context;
 
 // Convert HTML elements to JS object
 var submitButton = document.getElementById("submit");
@@ -14,6 +16,7 @@ var userElem = document.getElementById("userinput");
 var p2ErrorElem = document.querySelector('#player2 p.error');
 var p1ErrorElem = document.querySelector('#player1 p.error');
 var answerElem = document.getElementById("word");
+var canvas = document.getElementById("hangman");
 
 restartButton.addEventListener("click", restartGame);
 submitWordButton.addEventListener("click", createNewAnswer);
@@ -29,6 +32,28 @@ function init() {
 	document.getElementById("player2").style.display = "none";
     submitButton.addEventListener("click", validateInput);
 	//submitButton.addEventListener("click", compareAndDisplayOutcome);
+
+	canvas = document.getElementById("hangman");
+    isCanvas = false;
+
+	if (canvas.getContext) {
+		context = canvas.getContext('2d');
+		isCanvas = true;
+
+		// Draw the post
+		context.beginPath();
+		context.moveTo(56, 10);
+		context.lineTo(56, 0);
+		context.lineTo(170, 0);
+		context.lineTo(170, 243);
+		context.moveTo(112, 243);
+		context.lineTo(228, 243);
+		context.lineWidth = 2;
+		context.stroke();
+	} else {
+		isCanvas = false;
+		// Image of the post
+	}
 }
 
 function validateInput() {
@@ -115,10 +140,27 @@ function restartGame() {
 
 	// Reset the outcome text to three dots
 	outcomeElem.textContent = initialOutcome;
-	guessesRemaining = 5;
+	guessesRemaining = 6;
 	userElem.value = "";
 	allGuesses = [];
+	context.clearRect(0, 0, canvas.width, canvas.height);
 	init();
+
+}
+
+function drawHead() {
+
+	context.moveTo(85, 40);
+    context.arc(56, 40, 30, 0, Math.PI * 2, true); // circle for the head
+    context.stroke();
+	
+}
+
+function drawBody(startX, startY, lineX, lineY) {
+
+    context.moveTo(startX, startY);
+    context.lineTo(lineX, lineY);
+    context.stroke();
 
 }
 
@@ -153,14 +195,64 @@ function compareAndDisplayOutcome(userInput) {
 	} else if (duplicate) {
     	outcomeElem.textContent = "You already guessed this letter! Guess again.";
 	} else if (right) {
-        outcomeElem.textContent = "You guessed correctly!";
+        outcomeElem.textContent = "You guessed correctly. Keep it up!";
     } else if ( guessesRemaining > 0 ) {
-        outcomeElem.textContent = "Wrong, guess again! Guesses left: " + guessesRemaining;
-        guessesRemaining--;
-    } else {
-        outcomeElem.textContent = "You lose!!!";
-        //Remove the event listener to end game
-        submitButton.removeEventListener("click", validateInput);
+    	guessesRemaining--;
+
+    	if (guessesRemaining === 0) {
+    		outcomeElem.textContent = "You lose!!!";
+        	//Remove the event listener to end game
+        	submitButton.removeEventListener("click", validateInput);
+    	} else {
+    		outcomeElem.textContent = "Not quite. You have " + guessesRemaining + " guesses remaining.";
+    	}
+
+        switch (guessesRemaining) {
+        	case 5: // Head
+        		if (isCanvas) 
+        			drawHead();
+        		else {
+        			// Add Image
+        		}
+        		break;
+        	case 4: // Torso
+        		if (isCanvas) {
+        			drawBody(56, 70, 56, 153);
+        		}
+        		break;
+        	case 3: // Left Arm
+        		if (isCanvas)
+        			drawBody(56, 93, 10, 93);
+        		else {
+        			// Add image
+        		}
+        		break;
+        	case 2: // Right Arm
+        		if (isCanvas)
+        			drawBody(56, 93, 102, 93);
+        		else {
+        			// Add image
+        		}
+        		break;
+        	case 1: // Left Leg
+        		if (isCanvas)
+        			drawBody(56, 153, 10, 223);
+        		else {
+        			// add image
+        		}
+        		break;
+        	case 0: // Right Leg
+        		if (isCanvas) {
+        			drawBody(56, 153, 102, 223);
+        			context.closePath();
+        		} else {
+        			// Add image
+        		}
+        		break;
+        		
+        	default:
+        }
+
     }
 
 }
